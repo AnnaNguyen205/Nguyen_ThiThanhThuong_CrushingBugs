95fa606c51649145d8df861b0486b4c45f869d93
AnnaNguyen205 <156358256+AnnaNguyen205@users.noreply.github.com>
2024-07-01 23:44:54 -0400

    FIx bug 2 resetting
    In this commit, the bug of resetting and choosing a new puzzle was fixed to give user a fresh board to drop onto. The reset button was also given some CSS rules to make it look better.

--- js/main.js
+++ js/main.js
@@ -4,50 +4,69 @@ console.log("Javascript is connected");
 const theButtons = document.querySelectorAll("#buttonHolder img");
 const puzzleBoard = document.querySelector(".puzzle-board");
 const puzzlePieces = document.querySelectorAll(".puzzle-pieces img");
-const dropZones = document.querySelectorAll(".drop-zone");
+const dropZones = document.querySelectorAll(".drop-zone")
+    resetButton = document.querySelector('#resetBut');
+    // Bug#2: resetButton was created
 let draggedPiece;
 
 console.log(theButtons);
 console.log(puzzleBoard);
 
-//functions
-
 function changeBGImage(event) {
-    console.log("change BGImage called");
+    console.log("Change BGImage called");
     //Method 1
     // console.log(this.id);
     // background-image: url('../images/backGround0.jpg');
     // puzzleBoard.style.backgroundImage = `url('./images/backGround${this.id}.jpg')`;
 
     //Method 2
-    console.log(event.currentTarget.id);
+    console.log("Changing background image to", event.currentTarget.id);
     puzzleBoard.style.backgroundImage = `url('./images/backGround${event.currentTarget.id}.jpg')`;
+    
+    //Bug#2: Change the puzzle pieces following the background image
+    const puzzleId = event.currentTarget.id;
+    const pieceNames = ['topLeft', 'topRight', 'bottomLeft', 'bottomRight'];
+    
+    puzzlePieces.forEach((piece, index) => {
+        piece.src = `./images/${pieceNames[index]}${puzzleId}.jpg`;
+    });
+
+    resetPieces(); //Bug#2: resetPieces function was placed here to make previous pieces disappear when changing the background.
+}
+
+//this function is for bug#2
+function resetPieces() {
+    console.log("Resetting pieces to their original positions");
+    puzzlePieces.forEach(piece => document.querySelector('.puzzle-pieces').appendChild(piece));
 }
 
 function handleStartDrag() {
-    console.log(`Started dragging ${this}`);
+    console.log(`Started dragging piece: `, this);
     draggedPiece = this;
 }
 
 function handleOver(e) {
     e.preventDefault();
-    console.log("Dragged Over");
+    console.log("Dragged over a drop zone");
 }
 
 //Bug#1: Multiple puzzle pieces can be dropped into a single drop zone.
 //An if statement was added to prevent it.
 function handleDrop() {
-    console.log("Dropped a piece")
-
     if (this.children.length > 0) {
+        console.log("Drop zone already contains a piece. Drop action prevented.");
         return; // If there is already a piece in the drop zone, the action can't be done.
     }
+
     this.appendChild(draggedPiece);
+    console.log(`Dropped piece into drop zone`);
 }
 
 //eventListeners
 
 theButtons.forEach(button =>  button.addEventListener("click", changeBGImage));
+
+resetButton.addEventListener('click', resetPieces); //Bug#2: this was added to make previous pieces disappear when resetting.
 
 puzzlePieces.forEach(piece => piece.addEventListener("dragstart", handleStartDrag));
 
